d317c2cb755ceed76dc4099b73c21c0b3fc27409
vitus12345 <francis.vitus@gmail.com>
2023-11-05 04:00:20 -1200

    first commit

---
+++ src/components/Display.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navber from './Navigation/Navbar'
+import Footer from './Footer/Footer'
+const Display = () => {
+  return (
+    <div>
+        <Navber />
+        <Footer />
+    </div>
+  )
+}
+
+export default Display
+
+
+
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <React.Fragment>
+      <footer className ="bg-dark text-center text-white">
+       
+        <div className ="container p-4 pb-0">
+            
+            <section className ="mb-4">
+           
+            <a className ="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i className ="fab fa-facebook-f"></i></a>
+
+            
+            <a className ="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i className ="fab fa-twitter"></i></a>
+
+            
+            <a className ="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i className ="fab fa-google"></i></a>
+
+           
+            <a className ="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i className ="fab fa-instagram"></i></a>
+
+           
+            <a className ="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i className ="fab fa-linkedin-in"></i></a>
+
+           
+            <a className ="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i className ="fab fa-github"></i></a>
+            </section>
+            
+        </div>
+
+        <div className ="text-center p-3">
+            © 2020 Copyright:
+            <a className ="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>  
+        </footer>
+    </React.Fragment>
+  );
+}
+
+export default Footer
+
+
+
